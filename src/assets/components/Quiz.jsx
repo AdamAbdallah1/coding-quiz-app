670aa083105d967a9474040b349386c57cc1f20e
Adam Abdallah <adamabdallahayln1@gmail.com>
2025-07-03 17:15:43 +0300

    Creating a question bank of 3 questions

--- src/assets/components/Quiz.jsx
+++ src/assets/components/Quiz.jsx
@@ -1,6 +1,30 @@
 function Quiz() {
+    const questionBank = [
+        {
+            question: "What is the capital of France?",
+            options: ["Beirut", "London", "Paris", "Rome"],
+            answer: "Paris"
+        },
+        {
+            question: "Which language is used for web apps?",
+            options: ["PHP", "Python", "Javascript", "All"],
+            answer: "All"
+        },
+        {
+            question: "What does JSX stand for?",
+            options: [
+                "Javascript XML",
+                "Java Syntax Extention",
+                "Just a Simple Example",
+                "None of the above"
+            ],
+            answer: "Paris"
+        },
+
+    ]
+
     return (
-        ss
+        <div></div>
     )
 }
 
