ebd9514d59ff3e74d42ea2abfb55d37df5b9e227
Adam Abdallah <adamabdallahayln1@gmail.com>
2025-07-03 21:45:34 +0300

    Adding Child Quiz Component

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,17 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Quiz from './assets/components/Quiz'
 
 function App() {
   
 
   return (
     <div className='app-container'>
-      Hello World
+
+      <h1>Quiz App</h1>
+    
+      <Quiz />
     </div>
   )
 }
