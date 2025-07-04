3313c726ae6750efdaf86150a2faed4f8a77508d
Adam Abdallah <adamabdallahayln1@gmail.com>
2025-07-04 21:18:19 +0300

    Initailizing home page

---
+++ src/assets/components/Home.jsx
@@ -0,0 +1,11 @@
+function Home() {
+    return (
+        <div>
+            <h1>Quiz App</h1>
+            <p>Test your knowlegde</p>
+            <button>Start Quiz</button>
+        </div>
+    )
+}
+
+export default Home
