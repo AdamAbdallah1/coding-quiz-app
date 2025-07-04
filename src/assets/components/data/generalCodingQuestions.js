03f5fb6c8f531ba506d5cdd41bfe2f06c65ce63d
Adam Abdallah <adamabdallahayln1@gmail.com>
2025-07-04 22:06:57 +0300

    Typo in name

---
+++ src/assets/components/data/generalCodingQuestions.js
@@ -0,0 +1,19 @@
+const generalCodingQuestions = [
+  {
+    question: "What does IDE stand for?",
+    options: ["Internet Development Environment", "Integrated Development Environment", "Integrated Debug Environment", "Internal Design Environment"],
+    answer: "Integrated Development Environment"
+  },
+  {
+    question: "What does the acronym API stand for?",
+    options: ["Application Programming Interface", "Advanced Program Interaction", "Application Process Integration", "Automated Programming Interface"],
+    answer: "Application Programming Interface"
+  },
+  {
+    question: "Which one is NOT a programming paradigm?",
+    options: ["Object-Oriented", "Functional", "Procedural", "Circular"],
+    answer: "Circular"
+  }
+];
+
+export default generalCodingQuestions
