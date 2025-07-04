5e4dcf9a290f3b9d030f497f1d0577131c1f25b4
Adam Abdallah <adamabdallahayln1@gmail.com>
2025-07-04 21:14:28 +0300

    Initializing main page for quiz app

---
+++ src/assets/components/Main.jsx
@@ -0,0 +1,11 @@
+function Main() {
+    return (
+        <div>
+            <h1>Quiz App</h1>
+            <p>Test your knowlegde</p>
+            <button>Start Quiz</button>
+        </div>
+    )
+}
+
+export default Main
