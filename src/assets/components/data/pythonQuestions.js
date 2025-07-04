a13d139f878493e6461cf8a8f75c6d7b0c60f4a4
Adam Abdallah <adamabdallahayln1@gmail.com>
2025-07-04 21:39:47 +0300

    Initializing python questions

---
+++ src/assets/components/data/pythonQuestions.js
@@ -0,0 +1,19 @@
+const pythonQuestions = [
+  {
+    question: "Which keyword is used to define a function in Python?",
+    options: ["func", "def", "function", "define"],
+    answer: "def"
+  },
+  {
+    question: "Which data structure does Python use for key-value pairs?",
+    options: ["List", "Tuple", "Dictionary", "Set"],
+    answer: "Dictionary"
+  },
+  {
+    question: "What is the output of `print(2 ** 3)` in Python?",
+    options: ["5", "6", "8", "9"],
+    answer: "8"
+  }
+];
+
+export default pythonQuestions
