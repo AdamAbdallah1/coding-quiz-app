3633b13b9054c864febe0e7f8c8cf79a34e1e7d9
Adam Abdallah <adamabdallahayln1@gmail.com>
2025-07-04 22:23:36 +0300

    Fixing results using the prop returnToHome

--- src/assets/components/Results.jsx
+++ src/assets/components/Results.jsx
@@ -1,33 +1,38 @@
-function Results({ questionBank, userAnswers, restartQuiz }) {
-    function getScore() {
-        let finalScore = 0;
+import React from "react";
 
-        userAnswers.forEach((answer, index) => {
-            if (answer === questionBank[index].answer) {
-                finalScore ++
-            }
-        })
+function Results({ questionBank, userAnswers, restartQuiz, returnToHome }) {
 
-        return finalScore;
+  function getScore() {
+    let finalScore = 0;
+
+    userAnswers.forEach((answer, index) => {
+      if (answer === questionBank[index].answer) {
+        finalScore++;
+      }
+    });
+
+    return finalScore;
+  }
+
+  const score = getScore();
+
+  function calcResult() {
+    if (score >= 2) {
+      return "Quiz Completed";
+    } else {
+      return "Quiz Failed";
     }
-    const score = getScore()
+  }
 
-    function calcResult() {
-        if (score < 5) {
-            return "Quiz Failed"
-        } else {
-            return "Quiz Completed"
-        }
-    }
-
-    return (
-        <div>
-            <h2>Quiz Completed</h2>
-            <p>Your Score: {score}/{questionBank.length}</p>
-            <p>{calcResult()}</p>
-            <button className="restart-button" onClick={restartQuiz}> Restart Quiz</button>
-        </div>
-    )
+  return (
+    <div>
+      <h2>Quiz Completed</h2>
+      <p>Your Score: {score}/{questionBank.length}</p>
+      <p>{calcResult()}</p>
+      <button className="restart-button" onClick={restartQuiz}>Restart Quiz</button>
+      <button className="start-button" onClick={returnToHome}>Home Page</button>
+    </div>
+  );
 }
 
-export default Results
+export default Results;
