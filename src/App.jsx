301aaf255c5ac55b5344236211fcccac526937c0
Adam Abdallah <adamabdallahayln1@gmail.com>
2025-07-04 22:34:45 +0300

    Correcting directiores after changing it

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,8 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Quiz from './assets/components/Quiz'
-import Home from './assets/components/Home'
+import Quiz from './components/Quiz'
+import Home from './components/Home'
 
 function App() {
   
